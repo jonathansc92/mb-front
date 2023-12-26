46d9794a42970b2e4f4d29faa9bc2663418bd2e3
Jonathan Cruz <jonathansc92@gmail.com>
2023-12-26 13:48:29 -0300

    refactor: adicionado funções do router nas stores

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css';
 
-import { createApp } from 'vue';
+import { createApp, markRaw } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
@@ -14,7 +14,13 @@ const options = {
     // You can set your default options here
 };
 
-app.use(createPinia());
+const pinia = createPinia()
+
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
+
+app.use(pinia);
 app.use(router);
 app.use(Toast, options);
 
